778576f974f45fc55712904241508111e4c75bc9
NatashaStojanova <natashastojanova6@gmail.com>
2020-03-20 22:48:03 +0100

    show Profile properties

--- frontend/src/components/Explorer/explorer.js
+++ frontend/src/components/Explorer/explorer.js
@@ -14,11 +14,18 @@ class Explorer extends Component{
 
         this.state = {
             profile: newProfile,
+            profileFOAF: '',
+            friends: '',
+
         };
     }
 
     saveProfile = (e) => {
         PersonService.addProfile(this.state.profile).then(profileResp => {
+            //document.getElementById("foafExplorer").innerText = profileResp.name
+            this.setState({
+                profileFOAF: profileResp.data,
+            })
             alert("SENT DATA");
         }).catch(err => {
             alert(err)
@@ -42,7 +49,16 @@ class Explorer extends Component{
             <div className="container">
                 <Profile onProfileChange={this.profileChange}/>
                 <button type="button" className="btn btn-info" onClick={this.saveProfile}>Explore me</button>
+
+                <div align="left">
+                    <h5> First name: {this.state.profileFOAF.name}</h5><br/>
+                    <h5> Last name: {this.state.profileFOAF.surname}</h5><br/>
+                    <h5> Nickname: {this.state.profileFOAF.nick}</h5><br/>
+                    <h5> Title: {this.state.profileFOAF.title}</h5><br/>
+                    <h5> Homepage: {this.state.profileFOAF.homepage}</h5><br/>
+                </div>
             </div>
+
         )
     }
 }
